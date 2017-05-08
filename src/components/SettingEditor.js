b39e3d25e597e7783436436f0459d0ad120d6cf3
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-08 14:10:17 -0700

    Feat: Increment/Decrement Action on SettingEditor

--- src/components/SettingEditor.js
+++ src/components/SettingEditor.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+	incrementWorkTime, decrementWorkTime,
+	incrementRestTime, decrementRestTime
+} from '../actions/actions';
 
 // Component that allows for adjustments in break/work times
-const SettingEditor = props => {
+// onClick functions dispatch the proper decrement/increment actions based on
+// the components props
+let SettingEditor = (props) => {
 	return (
 		<div className="setting-editor text-center">
 			<p className="lead">
 				{props.text}
 			</p>
-			<button>-</button>
+			<button onClick={() => {
+				props.text === 'Break Length' ? props.dispatch(decrementRestTime()) : props.dispatch(decrementWorkTime());
+			}}>-</button>
 			<span>{props.length}</span>
-			<button>+</button>
+			<button onClick={() => {
+				props.text === 'Break Length' ? props.dispatch(incrementRestTime()) : props.dispatch(incrementWorkTime());
+			}}>+</button>
 		</div>
 	);
 };
+
+SettingEditor = connect()(SettingEditor);
 
 export default SettingEditor;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import pomodoroApp from './reducers/reducers';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
@@ -8,8 +9,17 @@ import './index.css';
 
 // Create the redux store
 let store = createStore(pomodoroApp);
+// Log the initial state
+console.log(store.getState())
 
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
